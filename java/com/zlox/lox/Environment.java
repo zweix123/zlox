c7bbdbc84b23a1574f310cf55c1d3749b7d10982
zweix123 <1979803044@qq.com>
2023-05-04 10:21:24 +0800

    add global environment

--- java/com/zlox/lox/Environment.java
+++ java/com/zlox/lox/Environment.java
@@ -4,13 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
     // 这里的key使用string而不是token，因为同一个名称在多个token中
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+
+        if (enclosing != null)
+            return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
         // 为什么将未定义操作放在运行时？因为如果在静态检查的错误，则前面的函数调用后面的函数则不行
     }
@@ -18,6 +31,11 @@ public class Environment {
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
