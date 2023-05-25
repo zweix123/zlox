57c4473db942190d83ba3bfdc5f8cd997f236956
zweix123 <1979803044@qq.com>
2023-05-25 11:25:00 +0800

    implement hash table

--- c/object.c
+++ c/object.c
@@ -16,24 +16,36 @@ static Obj *allocateObject(size_t size, ObjType type) {
     return object;
 }
 
-static ObjString *allocateString(char *chars, int length) {
+static ObjString *allocateString(char *chars, int length, uint32_t hash) {
     ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
     return string;
 }
 
-ObjString *takeString(char *chars, int length) {
-    // 不需要转移, 虚拟机使用, 有所有权
-    return allocateString(chars, length);
+static uint32_t hashString(const char *key, int length) { // FNV-1a
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
 }
 
-ObjString *copyString(const char *chars, int length) {
+ObjString *takeString(char *chars, int length) { // 虚拟机内部的拷贝
+    // 不需要转移, 虚拟机使用, 有所有权
+    uint32_t hash = hashString(chars, length);
+    return allocateString(chars, length, hash);
+}
+
+ObjString *copyString(const char *chars, int length) { // 转移所有权
     // 需要转移, 因为对于'虚拟机'来说, 没有所有权
+    uint32_t hash = hashString(chars, length);
     char *heapChars = ALLOCATE(char, length + 1);
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
-    return allocateString(heapChars, length);
+    return allocateString(heapChars, length, hash);
 }
 
 void printObject(Value value) {
